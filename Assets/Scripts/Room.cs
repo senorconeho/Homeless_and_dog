e527f698584bc46e9d570db3f4f867d357cc8e7b
SenorConeho <senorconeho@gmail.com>
2014-10-15 20:06:25 -0300

    Adding the 'resident' stuff
    - the resident is the person who lives in an apartment and can caught
    the dog robbing

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -11,9 +11,16 @@ public class Room : MonoBehaviour {
 	 * ==========================================================================================================
 	 */
 	// PUBLIC
-	public string			stRoomName;
+	public string			stRoomName;		//< Name of the room
 	Transform					trWindow;			//< Window of the room
 	Window						windowScript;	//< pointer to the window script
+
+	// Resident stuff
+	public Transform	trResidentGenerator;				//< Object pointing where to generate a resident
+	public Transform	trResidentPrefab;						//< Prefab of the resident itself
+	float			fResidentMinTimeToAppear = 3.0f;		//< The resident timer works this way: the room will randomize a value between min and max. When the timer is over, the resident will swipe the room and disappear. The game will randomize a new value and so forth
+	float			fResidentMaxTimeToAppear = 7.5f;
+	public float			fResidentCountdownTimer;
 
 	// PROTECTED
 
@@ -32,6 +39,8 @@ public class Room : MonoBehaviour {
 
 			windowScript = trWindow.gameObject.GetComponent<Window>();
 		}
+
+		trResidentGenerator = transform.Find("ResidentGenerator");
 		
 	}
 	
@@ -40,6 +49,8 @@ public class Room : MonoBehaviour {
 	/// <\summary>
 	void Start () {
 	
+		// Randomize the timer value
+		fResidentCountdownTimer = Random.Range(fResidentMinTimeToAppear, fResidentMaxTimeToAppear);
 	}
 	
 	/// <summary>
@@ -47,12 +58,27 @@ public class Room : MonoBehaviour {
 	/// <\summary>
 	void Update () {
 	
+		TickTimers();
 	}
 
 	/* ==========================================================================================================
 	 * CLASS METHODS
 	 * ==========================================================================================================
 	 */
+	/// <summary>
+	/// <\summary>
+	void TickTimers() {
+
+		fResidentCountdownTimer -= Time.deltaTime;
+
+		if(fResidentCountdownTimer <= 0.0f) {
+
+			// Regenerate the timer
+			fResidentCountdownTimer = Random.Range(fResidentMinTimeToAppear, fResidentMaxTimeToAppear);
+			// TODO: generate the resident and make him swipe the room
+		}
+	}
+
 	/// <summary>
 	/// <\summary>
 	public Transform FindChildrenByTag(string stTag, Transform tr) {
