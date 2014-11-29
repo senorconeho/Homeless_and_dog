be15fc7d88f507cbc12a56c985b40304522e725f
SenorConeho <senorconeho@gmail.com>
2014-11-29 09:18:49 -0200

    GUI and camera working on all resolutions
    Added a new script to adjust the cameras size to the resolution of the
    screen

---
+++ Assets/Scripts/AutoAdjustCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <class>
+/// Attempt to autoadjust the camera setting to not distort the gameboy' screen aspect ratio.
+/// Gameboy screen: 160px x 144px, scaled or not
+/// First, we have to set the orthographic camera size, and them update the x,y,w and h values on the 
+/// 'ViewportRect' property
+/// </class>
+public class AutoAdjustCamera : MonoBehaviour {
+
+	public int nScreenHeight;
+	public int nScreenWidth;
+	public float fAspectRatio;
+
+	public float fCameraSize;
+
+	Camera[]	cams;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get all cameras
+		cams = Camera.allCameras;	
+
+		// Get the current screen info
+		nScreenHeight = Screen.height;
+		nScreenWidth = Screen.width;
+
+		fAspectRatio = (float) nScreenWidth/nScreenHeight;
+
+		fCameraSize = 1.6f / fAspectRatio;
+
+		// Adjust all cameras size accordingly to the screen resolution
+		foreach(Camera cam in cams) {
+		
+			cam.orthographicSize = fCameraSize;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
